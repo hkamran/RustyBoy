634a922879ac236da4199ae0e66b7261119ee8c5
hkamran <hkamran490@gmail.com>
2020-12-27 20:22:11 -0500

    trying wasm.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,9 @@
-import("../pkg/index_bg.js").catch(console.error).then(wasm => {
-  var fileReader = new FileReader();
-  fileReader.onloadend = e => {
-    console.log(fileReader.result);
-    //wasm.load_buffer(new Uint8Array(fileReader.result))};
-    wasm.load_buffer(Array.from(new Uint8Array(fileReader.result)))};
-  var fileInputElement = document.getElementById("cartridge-input");
-  fileInputElement.addEventListener("change",
-    e => fileReader.readAsArrayBuffer(fileInputElement.files[0])
-  );
-});
+import("../pkg/index_bg.js")
+    .catch(console.error)
+    .then(wasm => {
+        window.wasm = wasm;
 
-fetch('index_bg.js')
-    .then(response => response.arrayBuffer())
-    .then(bytes => WebAssembly.instantiate(bytes, {}))
-    .then(results => {
-      alert(results.instance.exports.add_one(41));
+        let gameboy = wasm.Console.new();
+        console.log(gameboy);
     });
+
