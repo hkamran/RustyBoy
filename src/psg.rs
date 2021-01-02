398ba36a91e8a530dffb76c04159c0919b5f9f4b
Allen Ji <ajixuan11@gmail.com>
2021-01-01 23:58:42 -0500

    registers

--- src/psg.rs
+++ src/psg.rs
@@ -8,19 +8,16 @@ static VOLUME_LEVELS: usize = 0xF;
 //http://www.codeslinger.co.uk/pages/projects/mastersystem/sound.html
 #[wasm_bindgen]
 pub struct Psg {
+    ch0: [u8; 0x5], //channel 0: Tone and sweep
+    ch1: [u8; 0x5],// channel 1: Tone
+    ch2: [u8; 0x5],// channel 2: Wave
+    ch3: [u8; 0x5],// channel 3: Noise
+    // Control/ status
+    nr50: u8,
+    nr51: u8,
+    nr52: u8,
     volumes: [f32; 0xF],
-
-    //channel 0: Tone and sweep
-    ch0: [u8; 0x5],
-
-    // channel 1: Tone
-    ch1: [u8; 0x5],
-
-    // channel 2: Wave
-    ch2: [u8; 0x5],
-
-    // channel 3: Noise
-    ch3: [u8; 0x5]
+    wave_table: [u8; 0xF]
 }
 
 impl Psg {
@@ -30,6 +27,10 @@ impl Psg {
             ch1: [0x0; 0x5],
             ch2: [0x0; 0x5],
             ch3: [0x0; 0x5],
+            nr50: 0x00,
+            nr51: 0x00,
+            nr52: 0x00,
+            wave_table: [0x0; 0xF],
             volumes : {
                 let mut vol_table : [f32; 0xF] = [0.0; 0xF];
                 let mut curvol: f32 = MAX_VOLUME as f32;
@@ -54,6 +55,10 @@ impl Psg {
            0xFF10..=0xFF14 => { self.ch1[reg_index] = value },
            0xFF10..=0xFF14 => { self.ch2[reg_index] = value },
            0xFF10..=0xFF14 => { self.ch3[reg_index] = value },
+           0xFF24 => { self.nr50 = value },
+           0xFF25 => { self.nr51 = value },
+           0xFF26 => { self.nr52 = value },
+           0xFF30..=0xFF3F => { self.wave_table[ (address & 0x000F) as usize] = value },
            _ => {},
        }
    }
@@ -65,6 +70,10 @@ impl Psg {
            0xFF10..=0xFF14 => { self.ch1[reg_index] },
            0xFF10..=0xFF14 => { self.ch2[reg_index] },
            0xFF10..=0xFF14 => { self.ch3[reg_index] },
+           0xFF24 => { self.nr50 },
+           0xFF25 => { self.nr51 },
+           0xFF26 => { self.nr52 },
+           0xFF30..=0xFF3F => { self.wave_table[(address & 0x000F) as usize] },
            _ => { 0 },
        }
    }
